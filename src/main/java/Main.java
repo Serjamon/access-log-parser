67a88dbc9cdaab0696ffbd1957bfb608e4ef21db
Sergey Akimov <rsl2folly@gmail.com>
2024-07-11 20:35:12 +0300

    Tasks 1-10.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,17 +8,20 @@ public class Main {
             var dec = new DecimalFormat("#0.000");
 
             double res = fraction(x);
+            System.out.println("Задание №1");
             System.out.println("результат: " + dec.format(res));
         }
 
         //2
         {
+            System.out.println("Задание №2");
             int x = 992468;
             System.out.println(sumLastNums(x));
         }
 
         //3
         {
+            System.out.println("Задание №3");
             System.out.println(charToNum('0'));
             System.out.println(charToNum('4'));
             System.out.println(charToNum('9'));
@@ -26,6 +29,7 @@ public class Main {
 
         //4
         {
+            System.out.println("Задание №4");
             System.out.println(isPositive(-1));
             System.out.println(isPositive(0));
             System.out.println(isPositive(1));
@@ -40,6 +44,59 @@ public class Main {
             System.out.println(is2Digits(235));
             System.out.println(is2Digits(4235));
         }
+
+        //6
+        {
+            System.out.println("Задание №6");
+            System.out.println(isUpperCase('A'));
+            System.out.println(isUpperCase('f'));
+            System.out.println(isUpperCase('Z'));
+            System.out.println(isUpperCase('1'));
+        }
+
+        //7
+        {
+            System.out.println("Задание №7");
+            System.out.println(isInRange(10, 35, 22));
+            System.out.println(isInRange(10, 35, 2));
+            System.out.println(isInRange(35, 30, 35));
+            System.out.println(isInRange(5, 1, 3));
+            System.out.println(isInRange(2, 15, 33));
+        }
+
+        //8
+        {
+            System.out.println("Задание №8");
+            System.out.println(isDivisor (3, 6));
+            System.out.println(isDivisor (2, 15));
+            System.out.println(isDivisor (14, 7));
+            System.out.println(isDivisor (7, 21));
+        }
+
+        //9
+        {
+            System.out.println("Задание №9");
+            System.out.println(isEqual  (6, 6, 9));
+            System.out.println(isEqual  (3, 3, 3));
+            System.out.println(isEqual  (3, 6, 6));
+        }
+
+        //10
+        {
+            int sum=lastNumSum(5, 11);
+            System.out.println("Задание №10");
+            System.out.println(sum);
+            System.out.println(sum=lastNumSum(sum, 123));
+            System.out.println(sum=lastNumSum(sum, 14));
+            System.out.println(sum=lastNumSum(sum, 1));
+
+            System.out.println(lastNumSum(5, 11));
+            System.out.println(lastNumSum(lastNumSum(5, 11), 123));
+            System.out.println(lastNumSum(lastNumSum(lastNumSum(5, 11), 123), 14));
+            System.out.println(lastNumSum(lastNumSum(lastNumSum(lastNumSum(5, 11), 123), 14), 1));
+
+        }
+
 
     }
 
@@ -75,6 +132,41 @@ public class Main {
     //    5) Необходимо реализовать метод таким образом, чтобы он принимал положительное число x и возвращал true если оно двузначное.
     public static boolean is2Digits(int x) {
         return ((x / 10) != 0 && (x / 100) == 0);
+    }
+
+    //6) Необходимо реализовать метод таким образом, чтобы он принимал символ x
+    // и возвращал true если это большая буква в диапазоне от ‘A’ до ‘Z’.
+    // При реализации метода не использовать методы класса Character.
+    public static boolean isUpperCase(char x){
+        int symbolNum = (int)x;
+        return 64 < symbolNum && symbolNum < 91;
+    }
+
+    //7) Метод принимает левую и правую границу [a и b] некоторого числового диапазона.
+    // Необходимо реализовать метод таким образом, чтобы он возвращал true,
+    // если num входит в указанный диапазон (включая границы).
+    // Обратите внимание, что отношение a и b заранее неизвестно (неясно кто из них больше, а кто меньше)
+    public static boolean isInRange(int a, int b, int num){
+        return ((a<=b)&&(a<=num&&num<=b)) || ((a>b)&&(b<=num&&num<=a));
+    }
+
+    //8) Необходимо реализовать метод таким образом, чтобы он возвращал true
+    // если любое из принятых чисел делит другое нацело.
+    public static boolean isDivisor (int a, int b){
+        return a%b==0 || b%a==0;
+    }
+
+    //9) Необходимо реализовать метод таким образом, чтобы он возвращал true
+    // если все три полученных методом числа равны
+    public static boolean isEqual (int a, int b, int c){
+        return a==b && b==c;
+    }
+
+    //10) Выполните с его помощью последовательное сложение пяти чисел: 5, 11, 123, 14, 1,
+    // и результат выведите на экран. Постарайтесь выполнить задачу, используя минимально возможное
+    // количество вспомогательных переменных.
+    public static int lastNumSum(int a, int b){
+        return (a%10)+(b%10);
     }
 
 }
