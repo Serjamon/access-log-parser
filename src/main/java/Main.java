0a8ff62b955506f693f3d1d6fa8808ed64b86f54
Sergey Akimov <rsl2folly@gmail.com>
2024-07-15 21:30:32 +0300

    Added log file path check.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,46 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        int tryCount = 1;//"начиная с единицы" - увеличение произойдет после вывода сообщения
+
+        do {
+            System.out.println("Введите путь к log файлу: ");
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExist = file.exists();
+            boolean isDirectory = file.isDirectory();
+
+            if (isDirectory) {
+                System.out.println("Указан путь к директории, а не к файлу!");
+                continue;
+            }
+
+            if (fileExist) {
+                System.out.println("Путь указан верно.");
+                System.out.println("Это файл номер " + tryCount++);
+                //По условию останавливать ввод не требуется.
+                //break;
+            } else {
+                System.out.println("Указанный файл не существует!");
+            }
+
+        } while (true);
+
     }
+
+    //Переделал на переменные в самом методе main, как в условии задачи.
+
+//    //Проверим, существует ли файл с логами
+//    public static boolean fileExist(String path) {
+//        File file = new File(path);
+//        return file.exists();
+//    }
+//
+//    //Проверим, является ли введенный путь путем к директории, а не к файлу
+//    public static boolean isDirectory(String path) {
+//        File file = new File(path);
+//        return file.isDirectory();
+//    }
 }
